4dde09f6f9b70705163eb1c117424e65f8a24c39
swadiqueputhukkolli@gmail.com <swadiqueputhukkolli@gmail.com>
2026-08-20 05:09:34 +0000

    33 commit

--- asset_bundle_test_proj/src/data_processing.py
+++ asset_bundle_test_proj/src/data_processing.py
@@ -10,7 +10,7 @@
 # COMMAND ----------
 
 # DBTITLE 1,Get parameters
-# Parameters passed from the job 22
+# Parameters passed from the job 33333
 dbutils.widgets.text("catalog", "main", "Catalog Name")
 dbutils.widgets.text("schema", "default", "Schema Name")
 
